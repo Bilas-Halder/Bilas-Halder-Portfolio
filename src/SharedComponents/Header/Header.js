3a000f3aafc2a6b5c1ee00353694ff7628df75e4
Bilas-Halder <bilas8642@gmail.com>
2021-12-12 00:44:12 +0600

    nav-btn

--- src/SharedComponents/Header/Header.js
+++ src/SharedComponents/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Header.css';
+import { IoIosHome } from 'react-icons/io';
+import { FaProjectDiagram, FaBriefcase, FaUser } from 'react-icons/fa';
+import { RiMailOpenFill } from 'react-icons/ri';
 
 const Header = () => {
     return (
         <div className="header">
-
+            <div className="d-flex align-items-center h-100">
+                <div className="w-100">
+                    <ul>
+                        <li><a href="#" className="home nav-btn"><IoIosHome /></a></li>
+                        <li><a href="#" className="about nav-btn"><FaUser /></a></li>
+                        <li><a href="#" className="services nav-btn"><FaBriefcase /></a></li>
+                        <li><a href="#" className="projects nav-btn"><FaProjectDiagram /></a></li>
+                        <li><a href="#" className="contactMe nav-btn"><RiMailOpenFill /></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
